4b2b843f4655ec6d3f27e12b9085190e2fd30d53
Strelya72 <strelya72@gmail.com>
2019-05-17 14:20:47 +0300

    Game

--- index.php
+++ index.php
@@ -1,119 +1,137 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
-Released for free under a Creative Commons Attribution 2.5 License
-
-Name       : Endearing Green 
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20130425
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
+﻿<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Archivo+Narrow:400,700" rel="stylesheet" type="text/css">
-<link href="default.css" rel="stylesheet" type="text/css" media="all" />
-<!--[if IE 6]>
-<link href="default_ie6.css" rel="stylesheet" type="text/css" />
-<![endif]-->
-</head>
-<body>
-<div id="wrapper-bg">
-	<div id="wrapper">
-		<div id="header" class="container">
-			<div id="logo">
-				<h1><a href="#"><span>Endearing</span>Green</a></h1>
-			</div>
-			<div id="menu">
-				<ul>
-					<li class="active"><a href="#" accesskey="1" title="">Homepage</a></li>
-					<li><a href="#" accesskey="2" title="">Our Clients</a></li>
-					<li><a href="#" accesskey="3" title="">About Us</a></li>
-					<li><a href="#" accesskey="4" title="">Careers</a></li>
-					<li><a href="#" accesskey="5" title="">Contact Us</a></li>
-				</ul>
-			</div>
-		</div>
-		<div id="banner" class="container"> <img src="images/header-photo.jpg" width="1200" height="400" alt="" /> </div>
-		<div id="three-column" class="container">
-			<header>
-				<h2>Pellentesque tristique ante ut risus</h2>
-			</header>
-			<div class="tbox1">
-				<div class="box-style box-style01">
-					<div class="content"> <a href="#" class="image image-full"><img src="images/img01.jpg" alt=""></a>
-						<h2>Fusce ultrices fringilla</h2>
-						<p>Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. </p>
-						<a href="#" class="button-style">Learn More</a> </div>
-				</div>
-			</div>
-			<div class="tbox2">
-				<div class="box-style box-style02">
-					<div class="content"> <a href="#" class="image image-full"><img src="images/img02.jpg" alt=""></a>
-						<h2>Mauris vulputate dolor</h2>
-						<p>Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. </p>
-						<a href="#" class="button-style">Learn More</a> </div>
-				</div>
-			</div>
-			<div class="tbox3">
-				<div class="box-style box-style03">
-					<div class="content"> <a href="#" class="image image-full"><img src="images/img03.jpg" alt=""></a>
-						<h2>Nulla luctus eleifend </h2>
-						<p>Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh.</p>
-						<a href="#" class="button-style">Learn More</a> </div>
-				</div>
-			</div>
-		</div>
-		<div id="page">
-			<div id="content"></div>
-			<div id="sidebar"></div>
-		</div>
+    <meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Hill_IO</title>
+	
+	<!-- Standardised web app manifest -->
+	<link rel="manifest" href="appmanifest.json" />
+	
+	<!-- Allow fullscreen mode on iOS devices. (These are Apple specific meta tags.) -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon" sizes="256x256" href="icon-256.png" />
+	<meta name="HandheldFriendly" content="true" />
+	
+	<!-- Chrome for Android web app tags -->
+	<meta name="mobile-web-app-capable" content="yes" />
+	<link rel="shortcut icon" sizes="256x256" href="icon-256.png" />
+
+    <!-- All margins and padding must be zero for the canvas to fill the screen. -->
+	<style type="text/css">
+		* {
+			padding: 0;
+			margin: 0;
+		}
+		html, body {
+			background: #000;
+			color: #fff;
+			overflow: hidden;
+			touch-action: none;
+			-ms-touch-action: none;
+		}
+		canvas {
+			touch-action-delay: none;
+			touch-action: none;
+			-ms-touch-action: none;
+		}
+    </style>
+	
+
+</head> 
+ 
+<body> 
+	<div id="fb-root"></div>
+	
+	<script>
+	// Issue a warning if trying to preview an exported project on disk.
+	(function(){
+		// Check for running exported on file protocol
+		if (window.location.protocol.substr(0, 4) === "file")
+		{
+			alert("Exported games won't work until you upload them. (When running on the file:/// protocol, browsers block many features from working for security reasons.)");
+		}
+	})();
+	</script>
+	
+	<!-- The canvas must be inside a div called c2canvasdiv -->
+	<div id="c2canvasdiv">
+	
+		<!-- The canvas the project will render to.  If you change its ID, don't forget to change the
+		ID the runtime looks for in the jQuery events above (ready() and cr_sizeCanvas()). -->
+		<canvas id="c2canvas" width="854" height="480">
+			<!-- This text is displayed if the visitor's browser does not support HTML5.
+			You can change it, but it is a good idea to link to a description of a browser
+			and provide some links to download some popular HTML5-compatible browsers. -->
+			<h1>Your browser does not appear to support HTML5.  Try upgrading your browser to the latest version.  <a href="http://www.whatbrowser.org">What is a browser?</a>
+			<br/><br/><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx">Microsoft Internet Explorer</a><br/>
+			<a href="http://www.mozilla.com/firefox/">Mozilla Firefox</a><br/>
+			<a href="http://www.google.com/chrome/">Google Chrome</a><br/>
+			<a href="http://www.apple.com/safari/download/">Apple Safari</a></h1>
+		</canvas>
+		
 	</div>
-	<div id="footer-content" class="container">
-		<div id="fbox1">
-			<h2>Recent Updates</h2>
-			<ul class="style3">
-				<li class="first">
-					<p class="date"><a href="#">April<b>20</b></a></p>
-					<h3>Amet sed volutpat mauris</h3>
-					<p><a href="#">Mauris tempus nibh sodales adipiscing dolore.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">April<b>18</b></a></p>
-					<h3>Sagittis diam dolor sit amet</h3>
-					<p><a href="#">Duis arcu tortor fringilla sed  sed magna.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">April<b>15</b></a></p>
-					<h3>Adipiscing sed consequat</h3>
-					<p><a href="#">Pharetra ac velit sed in volutpat nisl mauris vel.</a></p>
-				</li>
-				<li>
-			</ul>
-		</div>
-		<div id="fbox2">
-			<h2>About</h2>
-			<p>This is<strong> Endearing Green</strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org">FCT</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/3/">Creative Commons Attributions 3.0</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :)</p>
-			<a href="#" class="button-style">Ipsum feugiat consequat</a> </div>
-		<div id="fbox3">
-			<h2>Contact</h2>
-			<p>Nam erat a posuere laoreet eget nibh sodales adipiscing. Phasellus tristique dui.</p>
-			<ul class="style5">
-				<li class="first"><span class="address">Address</span> <span class="address-01">1234 Somewhere Road #4285 <br />
-					Nashville, TN 00000</span> </li>
-				<li> <span class="mail">Mail</span> <span class="mail-01"><a href="#">someone@untitled.tld</a></span> </li>
-				<li> <span class="phone">Phone</span> <span class="phone-01">(000) 000-0000</span> </li>
-			</ul>
-		</div>
-	</div>
-</div>
-<div id="footer">
-	<p>Copyright (c) 2013 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org">FCT</a>. Photos by <a href="http://fotogrph.com/">Fotogrph</a>.</p>
-</div>
-</body>
-</html>
+	
+	<!-- Pages load faster with scripts at the bottom -->
+	
+	<!-- Construct 2 exported games require jQuery. -->
+	<script src="jquery-2.1.1.min.js"></script>
+
+	<script src="Photon-Javascript_SDK.min.js"></script>
+
+	
+    <!-- The runtime script.  You can rename it, but don't forget to rename the reference here as well.
+    This file will have been minified and obfuscated if you enabled "Minify script" during export. -->
+	<script src="c2runtime.js"></script>
+
+    <script>
+		// Start the Construct 2 project running on window load.
+		jQuery(document).ready(function ()
+		{			
+			// Create new runtime using the c2canvas
+			cr_createRuntime("c2canvas");
+		});
+		
+		// Pause and resume on page becoming visible/invisible
+		function onVisibilityChanged() {
+			if (document.hidden || document.mozHidden || document.webkitHidden || document.msHidden)
+				cr_setSuspended(true);
+			else
+				cr_setSuspended(false);
+		};
+		
+		document.addEventListener("visibilitychange", onVisibilityChanged, false);
+		document.addEventListener("mozvisibilitychange", onVisibilityChanged, false);
+		document.addEventListener("webkitvisibilitychange", onVisibilityChanged, false);
+		document.addEventListener("msvisibilitychange", onVisibilityChanged, false);
+		
+		function OnRegisterSWError(e)
+		{
+			console.warn("Failed to register service worker: ", e);
+		};
+		
+		// Runtime calls this global method when ready to start caching (i.e. after startup).
+		// This registers the service worker which caches resources for offline support.
+		window.C2_RegisterSW = function C2_RegisterSW()
+		{
+			if (!navigator.serviceWorker)
+				return;		// no SW support, ignore call
+			
+			try {
+				navigator.serviceWorker.register("sw.js", { scope: "./" })
+				.then(function (reg)
+				{
+					console.log("Registered service worker on " + reg.scope);
+				})
+				.catch(OnRegisterSWError);
+			}
+			catch (e)
+			{
+				OnRegisterSWError(e);
+			}
+		};
+    </script>
+</body> 
+</html> 
